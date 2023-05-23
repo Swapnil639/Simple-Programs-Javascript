4b49399d63b699397d6b3b85bedd09f526c216bf
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2023-05-23 18:26:12 +0530

    Added:Switch Case Problem

---
+++ SwitchCaseProgram/DayOfWeek.js
@@ -0,0 +1,24 @@
+let date = 5;
+switch(date){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+         console.log("Wenesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+}
